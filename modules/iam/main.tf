4f7f3960bbb8866e059f3048eb27df177ffab7c4
Gustavo Bressan <gus7avo182@gmail.com>
2025-05-26 23:15:42 -0300

    update: variables in each loop

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,50 +1,62 @@
 # CRIAR UM POLICE
 resource "aws_iam_policy" "custom_policy" {
-  for_each = var.aws_iam_policy
+    for_each = var.aws_iam_policy
 
-  name        = each.value.name
-  description = each.value.description
+    name        = each.value.name
+    description = each.value.description
 
-  policy = jsonencode(each.value.policy)
+    policy = jsonencode(each.value.policy)
 }
 
 # CRIAR UM REGRA
 resource "aws_iam_role" "custom_role" {
-  for_each = var.aws_iam_role
+    for_each = var.aws_iam_role
 
-  name = each.value.name
-  description = each.value.description
+    name        = each.value.name
+    description = each.value.description
 
-  assume_role_policy = jsonencode(each.value.assume_role_policy)
+    assume_role_policy = jsonencode(each.value.assume_role_policy)
 }
 
 # Anexa a policy personalizada ao role
 resource "aws_iam_role_policy_attachment" "attach_policy" {
-  role       = aws_iam_role.custom_role.name
-  policy_arn = aws_iam_policy.custom_policy.arn
+    for_each = var.aws_iam_role_policy_attachment
+
+    role       = each.value.role
+    policy_arn = each.value.policy_arn
 }
 
 # Cria um grupo IAM
 resource "aws_iam_group" "create_custom_group" {
-  name = "developers"
+    for_each = var.aws_iam_group
+
+    id      = each.value.id
+    arn     = each.value.arn
+    name    = each.value.name  
+    path    = each.value.path
+
 }
 
 # Anexa uma policy gerenciada pela AWS ao grupo
 resource "aws_iam_group_policy_attachment" "attachment_policy" {
-  group      = aws_iam_group.dev_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+    for_each = var.aws_iam_group_policy_attachment
+
+    group      = each.value.aws_iam_group_policy_attachment.group
+    policy_arn = each.value.aws_iam_group_policy_attachment.policy_arn
 }
 
 # Cria um usuário IAM
 resource "aws_iam_user" "create_user" {
-  name = "joao.silva"
+    for_each = var.aws_iam_user
+
+    name = each.value.name
 }
 
 # Adiciona o usuário ao grupo
 resource "aws_iam_user_group_membership" "add_user_to_group" {
-  user = aws_iam_user.dev_user.name
-  groups = [
-    aws_iam_group.dev_group.name
-  ]
+    for_each = var.aws_iam_user_group_membership
+
+    user    = each.value.aws_iam_user_group_membership.user
+    groups  = each.value.aws_iam_user_group_membership.groups
 }
 
--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,6 +1,7 @@
+# ====== POLICES =============================================
 variable "aws_iam_policy" {
-  description = "Políticas IAM a serem criadas"
-  type = map(object({
+  description   = "Políticas IAM a serem criadas"
+  type          = map(object({
     name        = string
     description = string
     statements  = list(object({
@@ -10,13 +11,66 @@ variable "aws_iam_policy" {
     }))
   }))
 }
+# ====== POLICES =============================================
 
+# ====== ROLES ===============================================
 variable "aws_iam_role" {
-  description = "Funções IAM a serem criadas"
-  type = map(object({
-    name        = string
-    description = string
-    assume_role_policy = string
+  description           = "Funções IAM a serem criadas"
+  type                  = map(object({
+    name                = string
+    description         = string
+    assume_role_policy  = string
   }))
-  
 }
+# ====== ROLES ===============================================
+
+# ====== ANEXAR POLICES A UMA ROLE ===========================
+variable "aws_iam_role_policy_attachment" {
+    description     = "Função que anexa uma política personalizada a uma função IAM"
+    type            = map(object({
+        role        = string
+        policy_arn  = string
+    }))
+}
+# ====== ANEXAR POLICES A UMA ROLE ===========================
+
+# ====== GRUPO IAM ===========================================
+variable "aws_iam_group" {
+    description = "Grupos IAM a serem criados"
+    type        = map(object({
+        id      = string
+        arn     = string
+        name    = string
+        path    = string
+    }))
+}
+# ====== GRUPO IAM ===========================================
+
+# ====== ANEXAR POLITICA CRIADA PELA AWS =====================
+variable "aws_iam_group_policy_attachment" {
+    description = "Anexa uma política gerenciada pela AWS a um grupo IAM"
+    type        = map(object({
+        group = string
+        policy_arn = string
+    }))
+}
+# ====== ANEXAR POLITICA CRIADA PELA AWS =====================
+
+# ====== CRIAR UM USUARIO ====================================
+variable "aws_iam_user" {
+    description = "Usuários IAM a serem criados"
+    type        = map(object({
+        name = string
+    }))
+}
+# ====== CRIAR UM USUARIO ====================================
+
+# ====== ADICIONAR UM USER A POLICE IAM ======================
+variable "aws_iam_user_group_membership" {
+    description = "Adiciona um usuário a um grupo IAM"
+    type        = map(object({
+        user      = string
+        groups     = list(string)
+    }))
+}
+# ====== ADICIONAR UM USER A POLICE IAM ======================
